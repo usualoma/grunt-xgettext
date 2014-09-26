f12de91d05fcf59d49b0617ef2408fca92fa8421
Arend van Beelen jr <arendjr@gmail.com>
2014-09-26 10:36:12 +0200

    Split extractors into separate files.

---
+++ tasks/extractors/html.js
@@ -0,0 +1,81 @@
+/*
+ * grunt-xgettext
+ * https://github.com/arendjr/grunt-xgettext
+ *
+ * Copyright (c) 2013-2014 Arend van Beelen, Speakap BV
+ * Licensed under the MIT license.
+ */
+
+"use strict";
+
+var grunt = require("grunt");
+var _ = require("lodash");
+
+/**
+ * Get all messages of a content
+ * @param  {String} content     content on which extract gettext calls
+ * @param  {Regex} regex        first level regex
+ * @param  {Regex} subRE        second level regex
+ * @param  {Regex} quoteRegex   regex for quotes
+ * @param  {String} quote       quote: " or '
+ * @param  {Object} options     task options
+ * @return {Object}             messages in a JS pot alike
+ *                                       {
+ *                                           singularKey: {
+ *                                               singular: singularKey,
+ *                                               plural: pluralKey,     // present only if plural
+ *                                               message: ""
+ *
+ *                                           },
+ *                                           ...
+ *                                       }
+ */
+function getMessages(content, regex, subRE, quoteRegex, quote, options) {
+    var messages = {}, result;
+
+    while ((result = regex.exec(content)) !== null) {
+        var strings = result[1],
+            singularKey = void 0;
+
+        while ((result = subRE.exec(strings)) !== null) {
+            var string = options.processMessage(result[1].replace(quoteRegex, quote));
+
+            // if singular form already defined add message as plural
+            if (typeof singularKey !== 'undefined') {
+                messages[singularKey].plural = string;
+            // if not defined init message object
+            } else {
+                singularKey = string;
+                messages[singularKey] = {
+                    singular: string,
+                    message: ""
+                };
+            }
+        }
+    }
+
+    return messages;
+}
+
+module.exports = function(file, options) {
+    var contents = grunt.file.read(file).replace("\n", " "),
+        fn = _.flatten([ options.functionName ]),
+        messages = {};
+
+    var extractStrings = function(quote, fn) {
+        var regex = new RegExp("" + fn + "\\(((?:" +
+            quote + "(?:[^" + quote + "\\\\]|\\\\.)+" + quote +
+            "\\s*)+)\\)", "g");
+        var subRE = new RegExp(quote + "((?:[^" + quote + "\\\\]|\\\\.)+)" + quote, "g");
+        var quoteRegex = new RegExp("\\\\" + quote, "g");
+
+        _.extend(messages, getMessages(contents, regex, subRE, quoteRegex, quote, options));
+    };
+
+    _.each(fn, function(func) {
+        extractStrings("'", func);
+        extractStrings('"', func);
+    });
+
+    return messages;
+};
--- tasks/xgettext.js
+++ tasks/xgettext.js
@@ -1,141 +1,21 @@
 /*
- * grunt-gettext
- * https://github.com/arendjr/grunt-gettext
+ * grunt-xgettext
+ * https://github.com/arendjr/grunt-xgettext
  *
- * Copyright (c) 2013 Arend van Beelen, Speakap BV
+ * Copyright (c) 2013-2014 Arend van Beelen, Speakap BV
  * Licensed under the MIT license.
  */
 
 "use strict";
 
-module.exports = function(grunt) {
+var _ = require("lodash");
 
-    var _ = grunt.util._;
+module.exports = function(grunt) {
 
     function escapeString(string) {
 
         return '"' + string.replace(/"/g, '\\"') + '"';
     }
-
-    /**
-     * Get all messages of a content
-     * @param  {String} content     content on which extract gettext calls
-     * @param  {Regex} regex        first level regex
-     * @param  {Regex} subRE        second level regex
-     * @param  {Regex} quoteRegex   regex for quotes
-     * @param  {String} quote       quote: " or '
-     * @param  {Object} options     task options
-     * @return {Object}             messages in a JS pot alike
-     *                                       {
-     *                                           singularKey: {
-     *                                               singular: singularKey,
-     *                                               plural: pluralKey,     // present only if plural
-     *                                               message: ""
-     *
-     *                                           },
-     *                                           ...
-     *                                       }
-     */
-    function getMessages(content, regex, subRE, quoteRegex, quote, options) {
-        var messages = {}, result;
-
-        while ((result = regex.exec(content)) !== null) {
-            var strings = result[1],
-                singularKey = void 0;
-
-            while ((result = subRE.exec(strings)) !== null) {
-                var string = options.processMessage(result[1].replace(quoteRegex, quote));
-
-                // if singular form already defined add message as plural
-                if (typeof singularKey !== 'undefined') {
-                    messages[singularKey].plural = string;
-                // if not defined init message object
-                } else {
-                    singularKey = string;
-                    messages[singularKey] = {
-                        singular: string,
-                        message: ""
-                    };
-                }
-            }
-        }
-
-        return messages;
-    }
-
-    var extractors = {
-        handlebars: function(file, options) {
-            var contents = grunt.file.read(file).replace("\n", " "),
-                fn = _.flatten([ options.functionName ]),
-                messages = {};
-
-            var extractStrings = function(quote, fn) {
-                var regex = new RegExp("\\{\\{\\s*" + fn + "\\s+((?:" +
-                    quote + "(?:[^" + quote + "\\\\]|\\\\.)+" + quote +
-                    "\\s*)+)[^}]*\\s*\\}\\}", "g");
-                var subRE = new RegExp(quote + "((?:[^" + quote + "\\\\]|\\\\.)+)" + quote, "g");
-                var quoteRegex = new RegExp("\\\\" + quote, "g");
-
-                _.extend(messages, getMessages(contents, regex, subRE, quoteRegex, quote, options));
-            };
-
-            _.each(fn, function(func) {
-                extractStrings("'", func);
-                extractStrings('"', func);
-            });
-
-            return messages;
-        },
-
-        html: function(file, options) {
-            var contents = grunt.file.read(file).replace("\n", " "),
-                fn = _.flatten([ options.functionName ]),
-                messages = {};
-
-            var extractStrings = function(quote, fn) {
-                var regex = new RegExp("" + fn + "\\(((?:" +
-                    quote + "(?:[^" + quote + "\\\\]|\\\\.)+" + quote +
-                    "\\s*)+)\\)", "g");
-                var subRE = new RegExp(quote + "((?:[^" + quote + "\\\\]|\\\\.)+)" + quote, "g");
-                var quoteRegex = new RegExp("\\\\" + quote, "g");
-
-                _.extend(messages, getMessages(contents, regex, subRE, quoteRegex, quote, options));
-            };
-
-            _.each(fn, function(func) {
-                extractStrings("'", func);
-                extractStrings('"', func);
-            });
-
-            return messages;
-        },
-
-        javascript: function(file, options) {
-            var contents = grunt.file.read(file).replace("\n", " ")
-                .replace(/"\s*\+\s*"/g, "")
-                .replace(/'\s*\+\s*'/g, "");
-
-            var fn = _.flatten([ options.functionName ]),
-                messages = {};
-
-            var extractStrings = function(quote, fn) {
-                var regex = new RegExp("(?:[^\\w]|^)" + fn + "\\s*\\(\\s*((?:" +
-                    quote + "(?:[^" + quote + "\\\\]|\\\\.)+" + quote +
-                    "\\s*[,)]\\s*)+)", "g");
-                var subRE = new RegExp(quote + "((?:[^" + quote + "\\\\]|\\\\.)+)" + quote, "g");
-                var quoteRegex = new RegExp("\\\\" + quote, "g");
-
-                _.extend(messages, getMessages(contents, regex, subRE, quoteRegex, quote, options));
-            };
-
-            _.each(fn, function(func) {
-                extractStrings("'", func);
-                extractStrings('"', func);
-            });
-
-            return messages;
-        }
-    };
 
     grunt.registerMultiTask("xgettext", "Extracts translatable messages", function() {
 
@@ -148,20 +28,16 @@ module.exports = function(grunt) {
         var translations = {};
 
         this.files.forEach(function(f) {
-
-            if (!extractors.hasOwnProperty(f.dest)) {
-                console.log("No gettext extractor for type: " + f.dest);
-                return;
-            }
+            var extractor = require("./extractors/" + f.dest);
 
             var messages = {};
             f.src.forEach(function(file) {
-                _.extend(messages, extractors[f.dest](file, options));
+                _.extend(messages, extractor(file, options));
             });
 
             _.extend(translations, messages);
 
-            var count = Object.keys(messages).length;
+            var count = _.keys(messages).length;
             grunt.log.writeln("Extracted " + count + " messages from " + f.dest + " files.");
         });
 
@@ -180,7 +56,7 @@ module.exports = function(grunt) {
 
         grunt.file.write(options.potFile, contents);
 
-        var count = Object.keys(translations).length;
+        var count = _.keys(translations).length;
         grunt.log.writeln(count + " messages successfully extracted, " +
             options.potFile + " written.");
 
